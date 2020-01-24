5092ee0179b8ff4c8dfe3b69fc46f66d04bb105b
Andrew Davis <atd285@gmail.com>
2020-01-23 19:42:06 -0500

    store configuration

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -7,7 +7,6 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import useStyles from './utils';
-
 // root of application, using nav bar component from material ui
 
 const App: React.FC = () => {
---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+import { createTypedHooks } from 'easy-peasy';
+import GuestBookStore from '../interfaces/Store';
+
+const typedHooks = createTypedHooks<GuestBookStore>();
